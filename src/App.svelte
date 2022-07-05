74fc6705bba2edaafc3e9064a9094b40a0037f1e
Wilbert Bocanegra <wilbertbocanegra@gmail.com>
2022-07-05 17:02:08 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { onMount } from "svelte";
+
+  /**
+   * @type {string}
+   */
+  let value;
+
+  /**
+   * @type {string}
+   */
+  let value2;
+
+  onMount(() => {
+    document.cookie = "auth=hola";
+    window.addEventListener("message", (e) => {
+      console.log(e);
+    });
+  });
+
+  const setCookie = () => {
+    document.cookie = `${value}=${value2}`;
+  };
+</script>
+
+<h1>HOLA</h1>
+<h1>LOGIN</h1>
+
+<input bind:value placeholder="key" />
+<input bind:value={value2} placeholder="name" />
+
+<button on:click={setCookie}>set</button>
